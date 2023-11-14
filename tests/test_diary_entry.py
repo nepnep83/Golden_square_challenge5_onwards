1c53ac77ac9dd01c1b83830418b49632d81b4bce
Thomas Powell <Thomas.powell838383@gmail.com>
2023-11-14 16:07:09 +0000

    Planned tests

---
+++ tests/test_diary_entry.py
@@ -0,0 +1,69 @@
+from lib.diary_entry import *
+
+"""
+Test that 2 arguments are passed into __init__()
+"""
+def test_init_receives_2_args():
+    #check type error is thrown
+    pass
+
+"""
+Test with correct inputs, returns correctly formatted diary entry
+"""
+def test_format_with_correct_args():
+    #check output is formatted correctly
+    pass
+
+"""
+Check that the correct word count is returned
+"""
+
+
+"""
+Check that no error is thrown when inputs are empty
+"""
+
+"""
+Check that the output is an int
+"""
+
+"""
+Check that correct time estimate is returned
+"""
+
+"""
+Check that estimated time is rounded correctly
+"""
+
+"""
+check that the output is an int
+"""
+
+"""
+Check that error is thrown when WPM is <= 0
+"""
+
+"""
+Check all contents is read in the time given
+"""
+
+"""
+Check that calling function for a second time does not return you to the start of the contents,
+but where you left off
+"""
+
+"""
+Check that no error is returned when not enough words are present in the contents
+"""
+
+"""
+Check that once all text is read, once called again, the user starts from the beginning.
+"""
+
+"""
+Check that error is thrown when WPM is <= 0
+"""
+
+"""
+Check that no error is thrown when minutes <= 0 
+"""
